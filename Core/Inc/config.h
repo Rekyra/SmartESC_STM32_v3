4be8183f515d952b1a420b9f404ee28536ef4d9f
Skoróbka <122433872+Rekyra@users.noreply.github.com>
2023-02-24 00:49:23 +0100

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -15,10 +15,10 @@
 #define PH_CURRENT_MAX_SPORT 55000
 
 // motor current limit for regen in mA
-#define REGEN_MAX_CURRENT 10000
+#define REGEN_MAX_CURRENT 20000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 10000 //max id
 
 // ADC channels
 #define ADC_VOLTAGE 0
